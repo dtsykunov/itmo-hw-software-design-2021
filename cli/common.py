58cf5ae1a9c637e6046ef524695ab982128a2b20
Dmitry Tsykunov <>
2021-02-21 16:21:16 +0300

    allow no args

--- cli/common.py
+++ cli/common.py
@@ -1,5 +1,7 @@
 class Command:
-    def __init__(self, name: str, args: list[str]):
+    def __init__(self, name: str, args: list[str] = None):
+        if args is None:
+            args = []
         self.name = name
         self.args = args
 
