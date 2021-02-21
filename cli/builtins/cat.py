4398016edd962d0c9c6ac9b69cce363aaf4231a3
Dmitry Tsykunov <>
2021-02-21 16:45:30 +0300

    change cat

--- cli/builtins/cat.py
+++ cli/builtins/cat.py
@@ -1,7 +1,6 @@
+import fileinput
 import sys
 
 if __name__ == "__main__":
-    for arg in sys.argv[1:]:
-        with open(arg, "r") as f:
-            for line in f:
-                print(line, end="")
+    for line in fileinput.input():
+        print(line, end="")
