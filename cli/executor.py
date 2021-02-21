5d1ff62ef361f02858c6587abe8e7760e8ee9d5a
Dmitry Tsykunov <>
2021-02-21 15:44:38 +0300

    reformat executor

--- cli/executor.py
+++ cli/executor.py
@@ -9,62 +9,60 @@ import sys
 from . import builtins
 from .common import Command, Pipeline
 
+_builtins = ["echo", "cat", "wc", "pwd"]
+
+
+def _isbuiltin(cmd: Command) -> bool:
+    return cmd.name in _builtins
+
+
+def _getbuiltinpath(name: str) -> str:
+    try:
+        with res.path(builtins, name + ".py") as p:
+            return str(p.resolve())
+    except FileNotFoundError as e:
+        logging.error(e)
+        raise
+
+
+def _exec(
+    cmd: Command, stdin: io.IOBase, stdout: io.IOBase, stderr: io.IOBase
+) -> (io.IOBase, io.IOBase):
+    return (
+        _exec_builtin(cmd, stdin, stdout, stderr)
+        if _isbuiltin(cmd)
+        else _exec_system(cmd, stdin, stdout, stderr)
+    )
+
+
+def _exec_builtin(cmd, stdin, stdout, stderr) -> (io.IOBase, io.IOBase):
+    assert _isbuiltin(cmd)
+    return sp.Popen(
+        [sys.executable, _getbuiltinpath(cmd.name)] + cmd.args,
+        stdin=stdin,
+        stdout=stdout,
+        stderr=stderr,
+    )
+
+
+def _exec_system(cmd: Command, stdin, stdout, stderr) -> (io.IOBase, io.IOBase):
+    return sp.Popen(
+        [cmd.name] + cmd.args,
+        stdin=stdin,
+        stdout=stdout,
+        stderr=stderr,
+    )
+
 
 class Executor:
-    _builtins = ["echo", "cat", "wc"]
-
-    @classmethod
+    @staticmethod
     def execute(
-        cls, pipeline: Pipeline, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr
+        pipeline: Pipeline, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr
     ) -> None:
         procs = []
         for cmd in pipeline.cmds[:-1]:
-            procs.append(p := cls._exec(cmd, p.stdout, sp.PIPE, sp.PIPE))
+            procs.append(p := _exec(cmd, p.stdout, sp.PIPE, sp.PIPE))
             stdin = p.stdout
-        procs.append(cls._exec(pipeline.cmds[-1], stdin, stdout, stderr))
+        procs.append(_exec(pipeline.cmds[-1], stdin, stdout, stderr))
         for proc in procs:
             proc.wait()
-
-    @classmethod
-    def _exec(
-        cls, cmd: Command, stdin: io.IOBase, stdout: io.IOBase, stderr: io.IOBase
-    ) -> (io.IOBase, io.IOBase):
-        return (
-            cls._exec_builtin(cmd, stdin, stdout, stderr)
-            if cls._isbuiltin(cmd)
-            else cls._exec_system(cmd, stdin, stdout, stderr)
-        )
-
-    @classmethod
-    def _isbuiltin(cls, cmd: Command) -> bool:
-        return cmd.name in cls._builtins
-
-    @classmethod
-    def _exec_builtin(cls, cmd, stdin, stdout, stderr) -> (io.IOBase, io.IOBase):
-        assert cls._isbuiltin(cmd)
-        return sp.Popen(
-            [sys.executable, cls._getbuiltinpath(cmd.name)] + cmd.args,
-            stdin=stdin,
-            stdout=stdout,
-            stderr=stderr,
-        )
-
-    @classmethod
-    def _getbuiltinpath(cls, name: str) -> str:
-        try:
-            with res.path(builtins, name + ".py") as p:
-                return str(p.resolve())
-        except FileNotFoundError as e:
-            logging.error(e)
-            raise
-
-    @classmethod
-    def _exec_system(
-        cls, cmd: Command, stdin, stdout, stderr
-    ) -> (io.IOBase, io.IOBase):
-        return sp.Popen(
-            [cmd.name] + cmd.args,
-            stdin=stdin,
-            stdout=stdout,
-            stderr=stderr,
-        )
