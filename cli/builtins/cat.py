a2a4bc6a834d3e1beb727a1128673bf26a5df9d1
Dmitry Tsykunov <>
2021-02-22 11:23:48 +0300

    fix cat

--- cli/builtins/cat.py
+++ cli/builtins/cat.py
@@ -2,16 +2,6 @@ import fileinput
 import sys
 
 if __name__ == "__main__":
-
-    # if len(sys.argv[:-1]) == 0:
-    #     for line in sys.stdin:
-    #         print(line)
-    #     sys.exit(0)
     for line in fileinput.input():
         print(line)
     sys.exit(0)
-
-    for arg in sys.argv[1:]:
-        with open(arg, "r") as f:
-            for line in f:
-                print(line, end="")
