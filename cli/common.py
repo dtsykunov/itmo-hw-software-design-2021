f51d659e7df1de675031dea7ac798450eec5591f
Dmitry Tsykunov <>
2021-02-22 18:31:19 +0300

    add comparison operators

--- cli/common.py
+++ cli/common.py
@@ -5,7 +5,17 @@ class Command:
         self.name = name
         self.args = args
 
+    def __eq__(self, other):
+        if isinstance(other, Command):
+            return self.name == other.name and self.args == other.args
+        return False
+
 
 class Pipeline:
     def __init__(self, cmds: list[Command]):
         self.cmds = cmds
+
+    def __eq__(self, other):
+        if isinstance(other, Pipeline):
+            return self.cmds == other.cmds
+        return False
