dbf2995c7a7bb144ffa6d597fc827a0abbb55838
Dmitry Tsykunov <>
2021-03-02 23:03:47 +0300

    update grep

--- cli/builtins/grep.py
+++ cli/builtins/grep.py
@@ -1,34 +1,37 @@
 import argparse as ap
+from io import IOBase
 
 import regex as re
 
-argparser = ap.ArgumentParser()
-argparser.add_argument(
+from ..common import Command
+
+_argparser = ap.ArgumentParser()
+_argparser.add_argument(
     "pattern",
     metavar="PATTERN",
     type=str,
     help="pattern that will be searched for",
 )
-argparser.add_argument(
+_argparser.add_argument(
     "files",
     metavar="FILE",
     type=str,
-    nargs="+",
+    nargs="*",
     help="files where the pattern will be searched for",
 )
-argparser.add_argument(
+_argparser.add_argument(
     "-i",
     dest="insensitive",
     action="store_true",
     help="case insensitive search",
 )
-argparser.add_argument(
+_argparser.add_argument(
     "-w",
     dest="literal",
     action="store_true",
     help="pattern is search for literally",
 )
-argparser.add_argument(
+_argparser.add_argument(
     "-A",
     dest="after",
     type=int,
@@ -37,23 +40,29 @@ argparser.add_argument(
 )
 
 
-if __name__ == "__main__":
-    args = argparser.parse_args()
+class Grep(Command):
+    def execute(self, env: dict, stdin: IOBase, stdout: IOBase, stderr: IOBase) -> None:
+        args = _argparser.parse_args(self.args)
+        flags = 0
+        pat = args.pattern
+        if args.insensitive:
+            flags |= re.IGNORECASE
+        if args.literal:
+            pat = re.escape(pat)
+        pattern = re.compile(pat, flags)
 
-    flags = 0
-    pat = args.pattern
-    if args.insensitive:
-        flags |= re.IGNORECASE
-    if args.literal:
-        pat = re.escape(pat)
-    pattern = re.compile(pat, flags)
+        if not args.files:
+            self._search(pattern, stdin, args.after, stdout)
+            return
+        for phyle in args.files:
+            with open(phyle, "r") as f:
+                self._search(pattern, f, args.after, stdout)
 
-    for phyle in args.files:
-        with open(phyle, "r") as f:
-            context = 0
-            for line in f:
-                if pattern.search(line):
-                    context = args.after + 1
-                if context != 0:
-                    print(line, end="")
-                    context -= 1
+    def _search(self, pattern, itr, after, stdout):
+        context = 0
+        for line in itr:
+            if pattern.search(line):
+                context = after + 1
+            if context != 0:
+                stdout.write(line)
+                context -= 1
